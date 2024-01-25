319f7fc2b518df4296c60af9b1c2ecf532aae50d
vitaliy <vitalily@mail.com>
2024-01-25 18:33:30 +0700

    homework 15

---
+++ src/main/java/ru/naumkin/homeworks/homework15/employee/Employee.java
@@ -0,0 +1,27 @@
+package ru.naumkin.homeworks.homework15.employee;
+
+public class Employee {
+    private final String name;
+    private final int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Employee(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
